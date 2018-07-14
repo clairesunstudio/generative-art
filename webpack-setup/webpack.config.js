4ad4a52bd6809548e035f12bcee2060dcbdad3a4
Minghua Sun <clairesunstudio@gmail.com>
2018-07-14 14:30:47 -0400

    clean dist

--- webpack-setup/webpack.config.js
+++ webpack-setup/webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -25,6 +26,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin('dist', {} ),
     new MiniCssExtractPlugin({
       filename: 'style.css',
     }),
