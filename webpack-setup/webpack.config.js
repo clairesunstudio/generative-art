f8d5e2c1b2f34aba8a8eb28357a16477affc07d0
Minghua Sun <clairesunstudio@gmail.com>
2018-07-14 14:22:25 -0400

    switch to mini css extract plugin

--- webpack-setup/webpack.config.js
+++ webpack-setup/webpack.config.js
@@ -1,7 +1,7 @@
 // webpack v4
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -20,17 +20,13 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract(
-          {
-            fallback: 'style-loader',
-            use: ['css-loader', 'sass-loader']
-          })
+        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin({
-      filename: 'style.css'
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
     }),
     new HtmlWebpackPlugin({
       inject: false,
