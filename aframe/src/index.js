01865041de715ca181c26068e928d0e50afec4bc
Minghua Sun <clairesunstudio@gmail.com>
2018-07-17 23:06:24 -0400

    Develop (#2)
    * create a new project using webpack-setup
    
    * first AR example
    
    * add jellyfish image
    
    * add build folder
    
    * add aframes
    
    * multiple index.html
    
    * wip
    
    * wip
    
    * wip

---
+++ aframe/src/index.js
@@ -0,0 +1,8 @@
+import 'aframe';
+import 'aframe-animation-component';
+import 'aframe-particle-system-component';
+import { ocean } from 'aframe-extras';
+import 'aframe-gradient-sky';
+import './scss/main.scss';
+
+console.log('aframe!');
